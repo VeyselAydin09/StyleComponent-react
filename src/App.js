eba84e77a27b3fa9a49971cf961f659d9e151828
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-24 19:22:43 +0300

    start app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+const style = {
+  color: {
+    header: "#fff",
+    body: "#eee",
+    footer: "#8A1c4a",
+  },
+  margins: {},
+  "max-width": "1000px",
+  width: "95vw",
+  responsive: "648px",
+};
+
+const App = () => {
+  return (
+    <>
+      <h1>App </h1>
+    </>
+  );
+};
+
+export default App;
